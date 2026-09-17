c3b3079b63196d0d25f74b2bb721674554902c16
vetrivel2203 <vetrivelpraj@gmail.com>
2026-09-17 18:06:29 +0530

    Add food menu and cart functionality

--- hotel.js
+++ hotel.js
@@ -1,1 +1,59 @@
+```javascript
+let foods = [
+    { name: "Pizza", price: 150 },
+    { name: "Burger", price: 100 },
+    { name: "Biryani", price: 180 },
+    { name: "Dosa", price: 60 }
+];
+
+let cart = [];
+
+function displayFoods() {
+
+    let output = "";
+
+    foods.forEach((food, i) => {
+        output += `
+            <div class="food">
+                <h3>${food.name}</h3>
+                <p class="price">₹${food.price}</p>
+                <button onclick="addToCart(${i})">
+                    Add to Cart
+                </button>
+            </div>
+        `;
+    });
+
+    document.getElementById("foods").innerHTML = output;
+}
+
+function addToCart(index) {
+
+    cart.push(foods[index]);
+
+    displayCart();
+}
+
+function displayCart() {
+
+    let output = "";
+    let total = 0;
+
+    cart.forEach(food => {
+
+        output += `
+            <div class="cart-item">
+                ${food.name} - ₹${food.price}
+            </div>
+        `;
+
+        total += food.price;
+    });
+
+    document.getElementById("cart").innerHTML = output;
+    document.getElementById("total").innerText = total;
+}
+
+displayFoods();
+```
 
